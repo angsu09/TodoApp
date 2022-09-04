fd42319f6a42f5db897de98494132e776c3f56d3
angsu09 <112499444+angsu09@users.noreply.github.com>
2022-09-04 22:55:42 +0200

    code change in taskList

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -2,27 +2,27 @@
     <div class="tasksList">
         <h2>List of Tasks</h2>
         <div class="row mb-3">
-            <label> {{selected.length}} out of {{tasks.length}} Tasks are selected</label>
+            <label> You have got {{completedTasks}} Tasks coming up in the next days.</label>
         </div>
         <table class="table table-responsive table-striped">
             <thead class="table-dark">
                 <tr>
-                    <th><label class="form-checkbox">
-                            <input type="checkbox" v-model="selectAll" v-on:click="selectAllTask()">
-                            <i class="form-icon"></i>
-                        </label></th>
+
                     <th>Task Decription</th>
+                    <th class="text-center ">Task Done</th>
                     <th class="text-center ">Delete</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="task of tasks" :key="task.id">
-                    <td id="taskCheckbox"><label class="form-checkbox">
-                            <input type="checkbox" :value="task.id" v-model="selected" v-on:change="selectTask()">
+
+                    <td for="taskCheckbox">{{task.description}}</td>
+                    <td id="taskCheckbox" class="text-center"><label class="form-checkbox">
+                            <input class="checkbox" type="checkbox" :disabled="task.completed" :value="task.id" v-model="task.completed"
+                                v-on:change="selectTask(task.id)">
                             <i class="form-icon"></i>
                         </label></td>
-                    <td class="description" for="taskCheckbox">{{task.description}}</td>
-                    <td class="text-center"><button class="btn btn-danger"
+                    <td class="text-center"><button class="btn btn-sm btn-danger"
                             v-on:click="deleteTask(task.id)">Delete</button></td>
                 </tr>
             </tbody>
@@ -39,44 +39,38 @@
         data() {
         return {
             tasks: [],
-            selected: [],
-            selectAll: false
+            completedTasks: 0,        
         };
     },
     async created() {
     try {
       const res = await axios.get(baseURL);
-
+    
       this.tasks = res.data;
+      this.tasks.map(task => {
+          if (!task.completed) {
+            this.completedTasks++
+        }
+    });
+
     } catch (e) {
       console.error(e);
     }
   },
   methods: {
-    selectAllTask() {
-		this.selected = [];
-		if (!this.selectAll) {
 
-        for (let i in this.tasks) {
-			this.selected.push(this.tasks[i].id);
-			}
-		}
-
-	},
-    selectTask() {
-        if(this.selected.length !== this.tasks.length) {
-            this.selectAll = false
-        }
-        else{
-            this.selectAll = true
-        }
+    async selectTask(taskId) {
+        await axios.patch(`${baseURL}/${taskId}`, {
+          completed: true
+        });
+        this.completedTasks--
     },
     async deleteTask(id) {
         try {
-        await axios.delete(baseURL +"/" + id)
+        await axios.delete(`${baseURL}/${id}`)
         let deletetaskId = this.tasks.map(task => task.id).indexOf(id)
         this.tasks.splice(deletetaskId,1)
-        if(this.selected.indexOf(id) >= 0 ) this.selected.splice(this.selected.indexOf(id),1)
+        // if(this.selected.indexOf(id) >= 0 ) this.selected.splice(this.selected.indexOf(id),1)
         } catch (e) {
             console.error(e); 
         }
@@ -85,5 +79,9 @@
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+    .checkbox{
+        width: 20px;
+        height: 20px;
+    }
 </style>
