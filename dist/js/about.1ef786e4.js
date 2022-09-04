8b9d36bd79d7790da6ca30ad29342b549d51413a
angsu09 <112499444+angsu09@users.noreply.github.com>
2022-09-04 19:43:04 +0200

    Uploading dist folder

---
+++ dist/js/about.1ef786e4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktaskcreationapp"]=self["webpackChunktaskcreationapp"]||[]).push([[443],{7381:function(a,n,t){t.r(n),t.d(n,{default:function(){return f}});var e=t(3396);const r={class:"about"},s=(0,e._)("h1",null,"This is an about page",-1),u=[s];function c(a,n){return(0,e.wg)(),(0,e.iD)("div",r,u)}var i=t(89);const o={},p=(0,i.Z)(o,[["render",c]]);var f=p}}]);
+//# sourceMappingURL=about.1ef786e4.js.map
